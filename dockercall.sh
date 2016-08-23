f59e5f0370e923f440f91f79633c1de70d19e94e
hemqa <hemant_chaudhari@persistent.co.in>
2016-08-23 16:00:44 +0530

    Updating dockersh file

--- dockercall.sh
+++ dockercall.sh
@@ -1,2 +1,20 @@
 #! /bin/bash
-docker exec -i $1 /bin/bash -c "sh /root/starttest.sh $1"
+
+ContainerName="$1"
+FolderName="$2"
+ContainerID=$(docker inspect --format {{.Id}} $ContainerName)
+
+if [ -n "$ContainerID" ] ; then
+    if [ -f  /var/lib/docker/image/aufs/layerdb/mounts/$ContainerID/mount-id ] ; then
+       ConID=$(cat /var/lib/docker/image/aufs/layerdb/mounts/$ContainerID/mount-id)
+       getmountID=/var/lib/docker/aufs/mnt/$ConID
+    fi
+    if [ ! -d "$getmountID" ] ; then
+        getmountID=/var/lib/docker/aufs/diff/$ContainerID
+    fi
+fi
+
+cp -R /var/lib/jenkins/workspace/$FolderName $getmountID/var/tmp/
+docker exec -i $1 /bin/bash -c "sh /var/tmp/$FolderName/starttest.sh /var/tmp/$FolderName"
+docker cp $ContainerName:/var/tmp/$FolderName/target /var/lib/jenkins/workspace/$FolderName/
+chown -fR jenkins:jenkins /var/lib/jenkins/workspace/$FolderName/target
--- starttest.sh
+++ starttest.sh
@@ -1,10 +1,6 @@
 #! /bin/bash
-kill -9 $(pgrep Xvfb)
-useradd jenkins
-chown -R jenkins:jenkins /root/*
 nohup Xvfb :10 -ac &
 export DISPLAY=:10
 firefox --safe-mode &
-mvn -f /root/pom.xml test
+mvn -f $1/pom.xml test
 kill -9 $(pgrep Xvfb)
-chown -R jenkins:jenkins /root/*
